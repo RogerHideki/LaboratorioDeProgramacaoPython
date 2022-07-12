cd0119dab9386de1f8e5619709ea59c7f51b3abf
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2022-07-12 19:49:35 -0400

    Completo

--- Aula02/ExercicioDeFixacao05.py
+++ Aula02/ExercicioDeFixacao05.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-tam = 5
+tam = 50
 
 vetorEntrada = np.zeros(tam)
 for i in range(tam):
     vetorEntrada[i] = float(input(f"V[{i + 1}]: "))
 vetorMedia = np.array([vetorEntrada.mean()] * tam)
 vetorDiferenca = abs(vetorEntrada - vetorMedia)
-print(f"O valor existente na lista que mais se aproxima do valor médio é: {vetorEntrada[vetorDiferenca.min().argmin]}")
+print(f"O valor existente na lista que mais se aproxima do valor médio é: {vetorEntrada[vetorDiferenca.argmin()]}")
